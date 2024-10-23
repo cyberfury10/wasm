525201da77dacc448d5f54dfcc3dca99b199b463
cyberfury10 <45261873+cyberfury10@users.noreply.github.com>
2024-10-23 17:18:30 +0530

    Add files via upload

---
+++ htmldiff-wasm/html_differ.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} old_html
+* @param {string} new_html
+* @returns {string}
+*/
+export function find_html_diff(old_html: string, new_html: string): string;
